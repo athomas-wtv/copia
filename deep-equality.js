8115fbb7db1b7b09482a1e6e5a0e1e24839b2de3
athomas-wtv <athomas.wtv@gmail.com>
2021-04-08 14:44:15 -0400

    Refactored and began deep dive equality check logic

--- deep-equality.js
+++ deep-equality.js
@@ -1,27 +1,42 @@
 const equals = (obj1, obj2) => {
-    let matchlenght = CheckKeyAndValueLengthMatch(obj1, obj2);
     let firstObjKeys = Object.keys(obj1);
     let secObjKeys = Object.keys(obj2);
-
-    if(firstObjKeys.length != secObjKeys.length)
-        return false;
 
     let firstObjValues = Object.values(obj1);
     let secObjValues = Object.values(obj2);
 
-    if(firstObjValues != secObjValues)
+    let keyArrayLengthMatch = checkArrayLengthMatch(firstObjKeys, secObjKeys);
+    let valueArrayLengthMatch = checkArrayLengthMatch(firstObjValues, secObjValues);
+
+    if(!keyArrayLengthMatch || !valueArrayLengthMatch)
         return false;
 
     let keysMatch = arraysMatch(firstObjKeys, secObjKeys);
     if(!keysMatch)
-    return false;
-    
-    let valuesMatch = arraysMatch(firstObjKeys, secObjKeys);
+        return false;
+        
+    let valuesMatch = arraysMatch(firstObjValues, secObjValues);
+    if(!valuesMatch)
+        return false;
+
     firstObjKeys.forEach(key)
 }
 
+const checkArrayLengthMatch = (arr1, arr2) => { return arr1.length == arr2.length }
+
 const arraysMatch = (arr1, arr2) => {
-    let match = arr1.every((el, idx) => el == arr2[idx]);
+    let match = arr1.every((el, idx) => {
+        if(typeof el != "Object" || typeof arr2[idx] != "Object")
+            el == arr2[idx];
+        else
+        {
+            let match = checkArrayLengthMatch(el, arr2);
+            if(!match)
+                return false;
+            
+            let keysMatch = arraysMatch(el, arr2)
+        }
+    });
     return match;
 }
 console.log(equals({"key1" : "value", "key2" : {"key3": 1}},
