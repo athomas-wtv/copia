8b17b63f9474c0444c84eeccb175672f53cc61d6
athomas-wtv <athomas.wtv@gmail.com>
2021-04-08 15:25:20 -0400

    Stopping point. Going to add comments and explanation

--- deep-equality.js
+++ deep-equality.js
@@ -9,17 +9,24 @@ const equals = (obj1, obj2) => {
     let keyArrayLengthMatch = checkArrayLengthMatch(firstObjKeys, secObjKeys);
     let valueArrayLengthMatch = checkArrayLengthMatch(firstObjValues, secObjValues);
 
+    console.log(1);
     if(!keyArrayLengthMatch || !valueArrayLengthMatch)
-        return false;
-
+    return false;
+    
+    console.log(2);
     let keysMatch = arraysMatch(firstObjKeys, secObjKeys);
     if(!keysMatch)
         return false;
     
+    console.log(3);
+    console.log(firstObjValues);
+    console.log(secObjValues);
     let valuesMatch = arraysMatch(firstObjValues, secObjValues);
+    console.log("3.5", valuesMatch);
     if(!valuesMatch)
         return false;
     
+    console.log(4);
     let objectsMatch = objMatchTest(obj1, firstObjKeys, obj2);
     return objectsMatch;
     
@@ -29,36 +36,34 @@ const checkArrayLengthMatch = (arr1, arr2) => { return arr1.length == arr2.lengt
 
 const objMatchTest = (obj1, obj1Keys, obj2) => {
     let match = obj1Keys.every(key => {
-        obj1[key] == obj2[key];
+        if(typeof obj1[key] != "Object" || typeof obj2[key] != "Object")
+            obj1[key] == obj2[key];
+        else
+            equals(obj1[key], obj2[key]);
     });
     return match;
 }
 
 const arraysMatch = (arr1, arr2) => {
+    // These two lines here is where I stopped
+    let ojectFoundInArr1 = objectCheck(arr1);
+    let ojectFoundInArr2 = objectCheck(arr2);
+    //////////////////////////////////////////
+    
     // This assumes that the array elements in both arrays will be in
     // the same position (i.e. the same index). More logic can be added
     // for the edge case of elements having different indexes with though the same value
-    let match = arr1.every((el, idx) => {
-        el == arr2[idx];
+    let match = arr1.every((el, idx, arr1) => {
+        return arr1[idx] == arr2[idx];
     });
+
     return match;
 }
 
-console.log(equals({"key1" : "value", "key2" : {"key3": 1}},
-                    {"key1" : "value", "key2" : {"key3": 1}}))
+const objectCheck = (arr) => { return arr.some(el => typeof el == "Object") }
 
-console.log(equals({"key1" : "value1", "key2" : {"key3": 1}, "key4" : "value2"},
-    {"key1" : "value", "key2" : {"key3": 1}}))
+console.log("should be true", equals({"key1" : "value", "key2" : {"key3": 1}},
+                                    {"key1" : "value", "key2" : {"key3": 1}}))
 
-
-
-    // if(typeof el != "Object" || typeof arr2[idx] != "Object")
-    //         el == arr2[idx];
-    //     else
-    //     {
-    //         let match = checkArrayLengthMatch(el, arr2);
-    //         if(!match)
-    //             return false;
-            
-    //         let keysMatch = arraysMatch(el, arr2)
-    //     }
+// console.log(equals({"key1" : "value1", "key2" : {"key3": 1}, "key4" : "value2"},
+//     {"key1" : "value", "key2" : {"key3": 1}}))
