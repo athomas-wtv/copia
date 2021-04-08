3a2f90bced2ff1a61967af20cef08d9513124792
athomas-wtv <athomas.wtv@gmail.com>
2021-04-08 08:10:44 -0400

    Added sync function and began helper function

--- copia-challange.js
+++ copia-challange.js
@@ -1,1 +1,18 @@
-console.log("It works");
+// For this question we want you to synchronize two lists. 
+// Each list contains only integers. The goal of the problem 
+// is to return the list of integers which are not contained in both lists.
+
+const sync = (arr1, arr2) => {
+    const uniqueElements = [];
+    
+    uniqueElements.push(findUniqueElements(arr1, arr2));
+    findUniqueElements(arr2, arr1)
+    
+
+}
+
+const findUniqueElements = (arrToCheck, control) => {
+    arrToCheck.forEach(el => {
+        
+    })
+}
